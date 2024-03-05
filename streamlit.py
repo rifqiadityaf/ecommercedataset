86a84cbf82ea2c41817b315d6dd72c7ba0544ae2
rifqiadityaf <162314663+rifqiadityaf@users.noreply.github.com>
2024-03-05 20:51:52 +0700

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,123 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+from funct import DataAnalyst
+sns.set(style='dark')
+
+all = pd.read_csv("data_all.csv")
+
+datetime_columns = ["order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date", "order_purchase_timestamp", "shipping_limit_date"]
+all.sort_values(by="order_approved_at", inplace=True)
+all.reset_index(inplace=True)
+ 
+for column in datetime_columns:
+    all[column] = pd.to_datetime(all[column])
+min_date = all["order_approved_at"].min()
+max_date = all["order_approved_at"].max()
+ 
+with st.sidebar:
+    st.title("Rifqi Aditya Fadhil")
+    
+    start_date, end_date = st.date_input(
+        label='Rentang Waktu',min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date]
+    )
+
+main_df = all[(all["order_approved_at"] >= str(start_date)) & 
+                 (all["order_approved_at"] <= str(end_date))]
+
+function = DataAnalyst(main_df)
+
+daily_orders_df = function.create_daily_orders_df()
+sum_spend_df = function.create_sum_spend_df()
+sum_order_items_df = function.create_sum_order_items_df()
+review_score, common_score = function.review_score_df()
+state, most_common_state = function.create_bystate_df()
+
+st.header('Dicoding E-Commerce Dashboard :convenience_store:')
+
+# Daily Orders
+st.subheader("Daily Orders")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    total_order = daily_orders_df["order_count"].sum()
+    st.markdown(f"Order Total: **{total_order}**")
+
+with col2:
+    total_revenue = format_currency(daily_orders_df["revenue"].sum(), "IDR", locale="id_ID")
+    st.markdown(f"Revenue Total: **{total_revenue}**")
+
+fig, ax = plt.subplots(figsize=(12, 6))
+ax.plot(
+    daily_orders_df["order_approved_at"],
+    daily_orders_df["order_count"],
+    marker="o",
+    linewidth=2,
+    color="#72BCD4"
+)
+ax.tick_params(axis="x", rotation=45)
+ax.tick_params(axis="y", labelsize=15)
+st.pyplot(fig)
+
+# Order Items
+st.subheader("Items Order")
+col1, col2 = st.columns(2)
+
+with col1:
+    total_items = sum_order_items_df["product_count"].sum()
+    st.markdown(f"Items Total: **{total_items}**")
+
+with col2:
+    avg_items = sum_order_items_df["product_count"].mean()
+    st.markdown(f"Items Average: **{avg_items}**")
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(45, 25))
+
+colors = ["#72BCD4", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+
+sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.head(5), palette=colors, ax=ax[0])
+ax[0].set_ylabel(None)
+ax[0].set_xlabel("Number of Sales", fontsize=30)
+ax[0].set_title("Produk yang paling laris", loc="center", fontsize=50)
+ax[0].tick_params(axis ='y', labelsize=35)
+ax[0].tick_params(axis ='x', labelsize=30)
+
+sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.sort_values(by="product_count", ascending=True).head(5), palette=colors, ax=ax[1])
+ax[1].set_ylabel(None)
+ax[1].set_xlabel("Number of Sales", fontsize=30)
+ax[1].invert_xaxis()
+ax[1].yaxis.set_label_position("right")
+ax[1].yaxis.tick_right()
+ax[1].set_title("Produk yang kurang laris", loc="center", fontsize=50)
+ax[1].tick_params(axis='y', labelsize=35)
+ax[1].tick_params(axis='x', labelsize=30)
+
+st.subheader("Customer Spend Money")
+col1, col2 = st.columns(2)
+
+with col1:
+    total_spend = format_currency(sum_spend_df["total_spend"].sum(), "IDR", locale="id_ID")
+    st.markdown(f"Total Spend: **{total_spend}**")
+
+with col2:
+    avg_spend = format_currency(sum_spend_df["total_spend"].mean(), "IDR", locale="id_ID")
+    st.markdown(f"Average Spend: **{avg_spend}**")
+
+fig, ax = plt.subplots(figsize=(12, 6))
+ax.plot(
+    sum_spend_df["order_approved_at"],
+    sum_spend_df["total_spend"],
+    marker="o",
+    linewidth=2,
+    color="#90CAF9"
+)
+ax.tick_params(axis="x", rotation=45)
+ax.tick_params(axis="y", labelsize=15)
+st.pyplot(fig)
+
+st.write('Sesuai pada grafik pertanyaan 1 dengan jelas menunjukkan bahwa produk yang kurang laris terjual adalah auto sedangkan sebaliknya produk yang paling laris terjual adalah bed_bath_table. Sesuai pada grafik pertanyaan 2 total budget yang dihabiskan customer pada bulan Januari - Mei tidak terjadi perubahan yang signifikan, setelah itu terjadi penurunan pada bulan Juni-September, kemudian kenaikan signifikan pada bulan Oktober-November, dan kembali menurun pada bulan Desember.')
