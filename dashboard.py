1408dc80afe49eaa20da7a5e145bac0d1a48a155
rifqiadityaf <162314663+rifqiadityaf@users.noreply.github.com>
2024-03-08 13:40:52 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,7 @@ import matplotlib.image as mpimg
 import seaborn as sns
 import streamlit as st
 import urllib
-from func import DataAnalyzer, BrazilMapPlotter
+from func import DataAnalyzer
 from babel.numbers import format_currency
 sns.set(style='dark')
 st.set_option('deprecation.showPyplotGlobalUse', False)
